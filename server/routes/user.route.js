e3fd2f64d165dcded7484165fb49257c2dbe1a67
huskyhehe <zhou.he1@northeastern.edu>
2022-07-26 14:53:56 -0700

    Ignore node_modules/

--- server/routes/user.route.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { home } = require("../controllers/user.controller");
-const userRouter = express.Router();
-
-userRouter.get("/user", home);
-
-module.exports = userRouter;
