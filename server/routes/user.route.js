78bd9dc0cff4f61a71b2e8e45df8794fb9217a7d
huskyhehe <zhou.he1@northeastern.edu>
2022-07-26 14:59:09 -0700

    Add user.route.js

---
+++ server/routes/user.route.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const { register } = require("../controllers/user.controller");
+const userRouter = express.Router();
+
+userRouter.post("/register", register);
+
+module.exports = userRouter;
